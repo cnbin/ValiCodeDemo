0452cd289a22db6a61af8a8f31709a367f25cf13
binbin <845040571@qq.com>
2015-08-10 10:12:06 +0800

    2015-8-10

---
+++ ValiCodeDemo/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="主页" Language="C#" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="ValiCodeDemo._Default" %>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title>验证码测试</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <center>
+    <fieldset>
+    <legend>验证码测试</legend>
+        请输入验证码：<asp:TextBox ID="valid" runat="server"></asp:TextBox>  
+        <asp:Image ID="Image1" runat="server" />
+        <asp:Button ID="Button1" runat="server" Text="提交" onclick="Button1_Click" />
+    </fieldset>
+    </center>
+    </form>
+</body>
+</html>
---
+++ ValiCodeDemo/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls.WebParts;
+
+
+namespace ValiCodeDemo
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Image1.ImageUrl = "ValiCode.aspx";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string ValiCode = valid.Text;
+            if (ValiCode == "")
+            {
+                Response.Write("<script>alert('验证码为空！');</script>");
+                valid.Focus();
+                return;
+            }
+            else if (ValiCode != (string)Session["ValiCode"])
+            {
+                Response.Write("<script>alert('输入的验证码错误');</script>");
+                valid.Text = "";
+                valid.Focus();
+            }
+            else
+            {
+                Response.Write("<script>alert('你输入的验证码正确');</script>");
+                valid.Text = "";
+                valid.Focus();
+            }
+        }
+    }
+}
